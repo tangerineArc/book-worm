2eb0b961b09f8d3b22b486dcd92893929c83b236
Swagatam <swagatamheroz@outlook.com>
2024-12-18 10:56:34 +0530

    feat: add theme-toggling functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 "use strict";
 
+setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+document.querySelector(".theme-toggler").addEventListener("click", event => {
+    setTheme(document.querySelector(".theme-toggler").id === "sun-icon");
+});
+
 const seeds = [
     ["The Da Vinci Code", "Dan Brown", "English", 480, true],
     ["Angels & Demons", "Dan Brown", "English", 480, true],
@@ -296,4 +302,22 @@ function Book(title, author, language, numPages, readStatus, id) {
     this.language = language;
     this.numPages = numPages;
     this.readStatus = readStatus;
+}
+
+function setTheme(checker) {
+    const themeToggler = document.querySelector(".theme-toggler");
+    const root = document.documentElement;
+
+    const drawSun = `<circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/>`;
+    const drawMoon = `<path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/>`;
+
+    if (checker) {
+        themeToggler.id = "moon-icon";
+        themeToggler.innerHTML = drawMoon;
+        root.className = "dark";
+    } else {
+        themeToggler.id = "sun-icon";
+        themeToggler.innerHTML = drawSun;
+        root.className = "light";
+    }
 }
